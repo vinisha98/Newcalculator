1e47d6aafe203957ce47d8ab86168d892a541dba
vishal sam <vinishakr9@gmail.com>
2022-01-21 20:24:24 +0530

    Added addition logic

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator{
 	public static void main(String[] args){
 		System.out.println("welcome to calculator code");
+		int a=5;
+		int b=5;
+		int sum=a+b;
+		System.out.println(sum);
 	}
 }
 
